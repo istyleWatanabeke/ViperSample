aea37a924d16e5df8f71fc8ac3759a1099a3b40d
WK-istyle <watanabeke@istyle.co.jp>
2019-10-29 16:19:28 +0900

    ルーティング実装中

---
+++ ViperSample/modules/RepositoryList/Router/RootRouter.swift
@@ -0,0 +1,22 @@
+//
+//  RootRouter.swift
+//  ViperSample
+//
+//  Created by 渡辺 健一 on 2019/10/29.
+//  Copyright © 2019 渡辺 健一. All rights reserved.
+//
+
+import UIKit
+
+class RootRouter {
+
+    private let window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func showFirstView() {
+
+    }
+}
---
+++ ViperSample/modules/RepositoryList/Router/RootRouterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootRouterViewController.swift
+//  ViperSample
+//
+//  Created by 渡辺 健一 on 2019/10/29.
+//  Copyright © 2019 渡辺 健一. All rights reserved.
+//
+
+import UIKit
+
+class RootRouterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
