8e3d460fafe6c962f42af12c136f80d09ff309a6
WK-istyle <watanabeke@istyle.co.jp>
2019-10-29 11:16:29 +0900

    Initial Commit

---
+++ ViperSampleTests/ViperSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ViperSampleTests.swift
+//  ViperSampleTests
+//
+//  Created by 渡辺 健一 on 2019/10/29.
+//  Copyright © 2019 渡辺 健一. All rights reserved.
+//
+
+import XCTest
+@testable import ViperSample
+
+class ViperSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
