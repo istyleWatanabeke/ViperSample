3336b42fd7b0b6c03752008920480503f79d01d5
WK-istyle <watanabeke@istyle.co.jp>
2019-10-30 11:35:33 +0900

    RepositoryDetailViewController作成

--- ViperSample/modules/RepositoryDetail/View/RepositoryDetailViewController.swift
+++ ViperSample/modules/RepositoryDetail/View/RepositoryDetailViewController.swift
@@ -7,24 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
-class RepositoryDetailViewController: UIViewController {
+protocol RepositoryDetailView: class {
+
+    func load(request urlRequest: URLRequest)
+}
+
+final class RepositoryDetailViewController: UIViewController {
+
+    var presenter: RepositoryDetailViewPresentation!
+
+    @IBOutlet private weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        presenter.viewDidLoad()
     }
+}
 
+extension RepositoryDetailViewController: RepositoryDetailView {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func load(request urlRequest: URLRequest) {
+        webView.load(urlRequest)
     }
-    */
-
 }
